83c02ae5613df8d5c8091e78f47306ba731a2ae5
KyleWyckoff <105688322+KyleWyckoff@users.noreply.github.com>
2025-03-09 10:01:56 -0700

    Update dining_philosopher.c
    added comments and timing

--- dining_philosopher.c
+++ dining_philosopher.c
@@ -17,94 +17,97 @@ void return_forks(int philosopher_number);
 void* philosopher(void* philosopher_number);
 void test(int philosopher_number);
 
-int state[total_ph];
-int phil[total_ph] = {0, 1, 2, 3, 4};
+int state[total_ph];                                                       //state = {'HUNGRY', 'EATING', 'THINKING'}
+int phil[total_ph] = {0, 1, 2, 3, 4};                                      //5 total philosophers
 
 sem_t mutex;
 sem_t S[total_ph];
 
 void test(int philosopher_number)
 {
-    if (state[philosopher_number] == HUNGRY
-        && state[LEFT] != EATING
+    if (state[philosopher_number] == HUNGRY                                 //check if the current philosopher is hungry and the 
+        && state[LEFT] != EATING                                            //forks to the left and right are not in use
         && state[RIGHT] != EATING) {
-        state[philosopher_number] = EATING;
-        sleep(2);
-        printf("Philosopher %d takes fork %d and %d\n",
-            philosopher_number + 1, LEFT + 1, philosopher_number + 1);
+        state[philosopher_number] = EATING;                                 //change state to eating
+        sleep(2);                                                           //sleep to perform action
+        printf("Philosopher %d takes fork %d and %d\n",                     //take forks based on eating state
+            philosopher_number + 1, LEFT + 1, philosopher_number + 1);      //and display to the terminal
 
-        printf("Philosopher %d is Eating\n", philosopher_number + 1);
-        sem_post(&S[philosopher_number]);
+        printf("Philosopher %d is Eating\n", philosopher_number + 1);       //Since the philosopher has forks, display to the terminal that they are eating
+        sem_post(&S[philosopher_number]);                                   //release the semaphore to allow other philosophers to act
     }
 }
 
 void pickup_forks(int philosopher_number){
-    sem_wait(&mutex);
-    state[philosopher_number] = HUNGRY;
-    printf("Philosopher %d is Hungry\n", philosopher_number+1);
-    test(philosopher_number);
-    sem_post(&mutex);
-    sem_wait(&S[philosopher_number]);
-    sleep(1);
+    sem_wait(&mutex);                                               //'locks' semaphore to allow changes to be made only to this philosopher
+    state[philosopher_number] = HUNGRY;                             //pickup forks changes state to hungryto convey it want to pick a fork up
+    printf("Philosopher %d is Hungry\n", philosopher_number+1);     //display to terminal our new state
+    test(philosopher_number);                                       //run test on current philosopher number
+    sem_post(&mutex);                                               //release the semaphore
+    sem_wait(&S[philosopher_number]);                               //lock the resources to the philosopher they were give to
+    sleep(1);                                                       //sleep to perform action
 
-    printf("Philosopher %d has forks %d and %d\n", philosopher_number + 1, LEFT + 1, philosopher_number + 1);
+    printf("Philosopher %d has forks %d and %d\n", philosopher_number + 1, LEFT + 1, philosopher_number + 1);   //Display to terminal which forks the philosopher picked up
 
 }
 
 void return_forks(int philosopher_number){
-    sem_wait(&mutex);  
-    state[philosopher_number] = THINKING;
+    sem_wait(&mutex);                                             //'locks' semaphore to allow changes to be made only to this philosopher
+    state[philosopher_number] = THINKING;                         //return forks changes state to thinking in order to make decision about left and right
     printf("Philosopher %d putting fork down %d and %d down\n", 
         philosopher_number+1, LEFT+1, philosopher_number+1);
-    printf("Philosopher %d is thinking\n", philosopher_number+1);
-    test(LEFT);
-    test(RIGHT);
-    sem_post(&mutex);
-    printf("Philosopher %d returned forks %d and %d\n", philosopher_number + 1, LEFT + 1, philosopher_number + 1);
+    printf("Philosopher %d is thinking\n", philosopher_number+1); //displays to terminal philosopher # is thinking at the moment
+    test(LEFT);                                                   //test for left fork
+    test(RIGHT);                                                  //test for right fork
+    sem_post(&mutex);                                             //'unlokcs' the mutex
+    printf("Philosopher %d returned forks %d and %d\n", philosopher_number + 1, LEFT + 1, philosopher_number + 1); //displays to terminal which forks the philosopher is putting dowm
 
 }
 
 void* philosopher(void* philosopher_number){
+    //variables used to capture different time measurements for the philosophers actions
     int think_time, eat_time;
     clock_t think_start, think_end, eat_start, eat_end;
     while(1){
-        int* i = philosopher_number;
+        int* i = philosopher_number; //captures the current philosopher
 
         //Time the philosopher takes to think
-        think_time = rand() % 3 + 1;
-        think_start = clock();
-        printf("Philosopher %d is thinking for %d seconds.\n", philosopher_number, think_time);
-        sleep(think_time);
-        think_end = clock();
+        think_time = rand() % 10 + 1;       //creates a random amount of time the philosopher will eat for
+        think_start = clock();              //ends clock to capture full time the philosopher will be thinking for
+        printf("Philosopher %d is thinking for %d seconds.\n", *i, think_time); //displays how long the philosopher is thinking to the terminal
+        sleep(think_time);                  //sleeps for the randomly generated amount of time
+        think_end = clock();                //ends clock to capture full time the philosopher was thinking for
         double think_duration = ((double)(think_end - think_start)) / CLOCKS_PER_SEC;
-        printf("Philosopher %d thought for %.2f seconds.\n", philosopher_number, think_duration);
-        pickup_forks(*i);
+        printf("Philosopher %d thought for %.2f seconds.\n", *i, think_duration); //displays how long the philosopher thought to the terminal
+        pickup_forks(*i);                   //philosopher will see if he can pick up forks to their left and right
 
-        eat_time = rand() % 5 + 1;
-        eat_start = clock();
-        printf("Philosopher %d is eating for %d seconds.\n", philosopher_number, eat_time);
-        sleep(eat_time);
-        eat_end = clock();
+        eat_time = rand() % 10 + 1;         //creates a random amount of time the philosopher will eat for
+        eat_start = clock();                //starts the clock to be able to capture time
+        printf("Philosopher %d is eating for %d seconds.\n", *i, eat_time); //displays how long the philosopher is eating to the terminal
+        sleep(eat_time);                    //sleeps for the randomly generated amount of time
+        eat_end = clock();                  //ends clock to capture full time the philosopher ate for
         double eat_duration = ((double)(eat_end - eat_start)) / CLOCKS_PER_SEC;
-        printf("Philosopher %d ate for %.2f seconds.\n", philosopher_number, eat_duration);
-        return_forks(*i);
+        printf("Philosopher %d ate for %.2f seconds.\n", *i, eat_duration);     //displays how long the philosopher ate to the terminal
+        return_forks(*i);                   //philosopher will see if they can return the forks to their left and right
     }
 }
 
 int main(){
     int i;
     pthread_t thread_id[total_ph];
-
-    sem_init(&mutex, 0, 1);
+    //initialize the semaphores
+    sem_init(&mutex, 0, 1);         
     for (i = 0; i < total_ph; i++){
         sem_init(&S[i], 0, 0);
     }
-
+    //Create threads for each philosopher and display thinking
     for (i = 0; i < total_ph; i++){
         pthread_create(&thread_id[i], NULL, philosopher, &phil[i]);
         printf("Philosopher %d is thinking\n", i + 1);
     }
 
+    
+    //Join threads 
     for (i = 0; i < total_ph; i++){
         pthread_join(thread_id[i], NULL);
     }
